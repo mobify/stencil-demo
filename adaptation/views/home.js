4c07ef888c49b53d3bf95699c3f22949fee5301a
Aveline Thelen <aveline@mobify.com>
2015-04-23 15:52:43 -0700

    Product tiles content

--- adaptation/views/home.js
+++ adaptation/views/home.js
@@ -11,15 +11,45 @@ function($, BaseView, template) {
             templateName: 'home',
             title: 'Cooking & Baking',
             firstp: 'If you are a budding baker, you can enjoy making your favourite cakes and recipes with cake tins and pans as well as all the accessories you need. View our Great British Bakeware range for the world’s leading manufacturer of superior bakeware.',
-            product: {
-                title: 'Wooden Chopping Board',
-                photo: {
-                    src: 'http://placehold.it/400x520',
-                    alt: 'Wooden Chopping Board'
+            product: [
+                {
+                    title: 'Wooden Chopping Board',
+                    photo: {
+                        src: 'http://placehold.it/400x520',
+                        alt: 'Wooden Chopping Board'
+                    },
+                    regularPrice: '$12.99',
+                    gridClass: 'c-grid__span'
                 },
-                regularPrice: '$12.99',
-                gridClass: 'c-grid__span'
-            },
+                {
+                    title: 'Set Of 3 Cookie Cutters',
+                    photo: {
+                        src: 'http://placehold.it/400x520',
+                        alt: 'Set Of 3 Cookie Cutters'
+                    },
+                    regularPrice: '$9.99',
+                    salePrice: '$4.99',
+                    gridClass: 'c-grid__span'
+                },
+                {
+                    title: 'Set of 4 Kitchen Measuring Scoops',
+                    photo: {
+                        src: 'http://placehold.it/400x520',
+                        alt: 'Set of 4 Kitchen Measuring Scoops'
+                    },
+                    regularPrice: '$5.99',
+                    gridClass: 'c-grid__span'
+                },
+                {
+                    title: 'Silicone Whisk',
+                    photo: {
+                        src: 'http://placehold.it/400x520',
+                        alt: 'Silicone Whisk'
+                    },
+                    regularPrice: '$3.99',
+                    gridClass: 'c-grid__span'
+                }
+            ],
             breadcrumbs: [
                 {
                     label: 'Home',
