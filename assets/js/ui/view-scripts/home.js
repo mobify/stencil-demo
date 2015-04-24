d7f1e52eba2c831e72660114a1bf3f3d39cb7c25
fractaltheory <michael@mobify.com>
2015-04-24 13:07:30 -0700

    Set up a pre-demo environment

--- assets/js/ui/view-scripts/home.js
+++ assets/js/ui/view-scripts/home.js
@@ -3,10 +3,9 @@ define([
 ],
 function($) {
     var homeUI = function() {
-        // Add any scripts you would like to run only on the home page here
-        $('.js-header-nav__toggle').on('click', function() {
-            $('.c-main-menu').trigger('main-menu:toggle');
-        });
+        // $('.js-header-nav__toggle').on('click', function() {
+        //     $('.c-main-menu').trigger('main-menu:toggle');
+        // });
     };
 
     return homeUI;
